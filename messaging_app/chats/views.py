ef05e203db8a8f8851e46bbae775cd02e87b04a1
Miheret Girmachew <124549518+Miheret-Girmachew@users.noreply.github.com>
2025-06-30 15:26:52 +0300

    fix : amendement on project structure

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,13 +1,10 @@
-# messaging_app/chats/views.py
-
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
 
-from .models import Conversation
-from .serializers import ConversationSerializer
-from .permissions import IsParticipantInConversation 
-
+from .models import Conversation 
+from .serializers import ConversationSerializer 
+from .auth import IsOwnerOrParticipant
 class ConversationDetailView(generics.RetrieveAPIView):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
-    permission_classes = [IsAuthenticated, IsParticipantInConversation]
+    
+    permission_classes = [IsOwnerOrParticipant]
